5dea29547af8f404c9725dc847b32c87e3c01ec3
iassolutions-it <131358767+iassolutions-it@users.noreply.github.com>
2023-05-07 18:15:04 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ const path = require('path')
 export default {
   root: path.resolve(__dirname, 'src'),
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    base: '/tp_integrador_fe-cac/',
   },
   server: {
     port: 8080,
